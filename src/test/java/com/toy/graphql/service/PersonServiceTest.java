11abcb5db92e67fd486f34af122c910a1a4b9f0a
99 <hahahoho5915@gmail.com>
2022-08-22 20:05:20 +0900

    add PersonService 테스트 코드

---
+++ src/test/java/com/toy/graphql/service/PersonServiceTest.java
@@ -0,0 +1,61 @@
+package com.toy.graphql.service;
+
+
+import com.toy.graphql.exception.GraphQLNotFoundException;
+import com.toy.graphql.model.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class PersonServiceTest {
+
+	@Autowired
+	private PersonService personService;
+
+	@DisplayName("findAll 테스트")
+	@Test
+	void findAll() {
+		// given
+		// when
+		List<Person> personList = personService.findAll();
+
+		// then
+		assertThat(personList.size()).isGreaterThan(0);
+	}
+
+	@DisplayName("findById 테스트")
+	@Test
+	void findById() {
+		// given
+		Integer id = 1;
+
+		// when
+		Person person = personService.findById(id);
+
+		// then
+		assertThat(person.getId()).isEqualTo(id);
+		assertThat(person.getFirstName()).isEqualTo("Corn");
+	}
+
+	@DisplayName("findById 테스트 - No data")
+	@Test
+	void findById2() {
+		// given
+		Integer id = 999;
+
+		// then
+		Assertions.assertThrows(GraphQLNotFoundException.class, () -> {
+			// when
+			Person person = personService.findById(id);
+		});
+	}
+}
