71e47e7acf79eda790566234729591520f56dd2d
99 <hahahoho5915@gmail.com>
2022-08-23 20:48:40 +0900

    modify jpa N+1 발생 @EntityGraph 적용

--- src/main/java/com/toy/graphql/repository/PersonRepository.java
+++ src/main/java/com/toy/graphql/repository/PersonRepository.java
@@ -1,7 +1,13 @@
 package com.toy.graphql.repository;
 
 import com.toy.graphql.entity.Person;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+	@Override
+	@EntityGraph(attributePaths = {"address"})
+	List<Person> findAll();
 }
