d5b6a8daf60babfad4fecb513a91838c93914c4f
99 <hahahoho5915@gmail.com>
2022-09-08 17:36:40 +0900

    modify 샘플데이터 생성 로직 변경

--- src/main/java/com/toy/graphql/data/SampleDataLoader.java
+++ src/main/java/com/toy/graphql/data/SampleDataLoader.java
@@ -2,6 +2,7 @@ package com.toy.graphql.data;
 
 import com.toy.graphql.entity.Address;
 import com.toy.graphql.entity.Person;
+import com.toy.graphql.repository.AddressRepository;
 import com.toy.graphql.repository.PersonRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -9,26 +10,45 @@ import org.springframework.stereotype.Component;
 @Component
 public class SampleDataLoader implements CommandLineRunner {
 	private final PersonRepository personRepository;
+	private final AddressRepository addressRepository;
 
-	public SampleDataLoader(PersonRepository personRepository) {
+	public SampleDataLoader(PersonRepository personRepository, AddressRepository addressRepository) {
 		this.personRepository = personRepository;
+		this.addressRepository = addressRepository;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Address address = Address.builder()
+				.address("Street")
+				.city("City")
+				.state("State")
+				.zip("99999")
+				.build();
+		addressRepository.save(address);
+		Address address2 = Address.builder()
+				.address("st")
+				.city("99City")
+				.state("-")
+				.zip("90081")
+				.build();
+		addressRepository.save(address2);
+		Address address3 = Address.builder()
+				.address("Qst")
+				.city("Wind-City")
+				.state("QWER")
+				.zip("030")
+				.build();
+		addressRepository.save(address3);
+
+
 		// 임시 데이터 1
 		Person person = Person.builder()
 				.firstName("Corn")
 				.lastName("kk99")
 				.phoneNumber("010.9999.9999")
 				.email("gamil@gmail.com")
-				.address(Address.builder()
-						.address("Street")
-						.city("City")
-						.state("State")
-						.zip("99999")
-						.build()
-				)
+				.address(address)
 				.build();
 		personRepository.save(person);
 
@@ -38,12 +58,7 @@ public class SampleDataLoader implements CommandLineRunner {
 				.lastName("Kim")
 				.phoneNumber("010.0000.0000")
 				.email("gg@gg.com")
-				.address(Address.builder()
-						.address("st")
-						.city("99City")
-						.state("-")
-						.zip("90081")
-						.build())
+				.address(address2)
 				.build();
 		personRepository.save(person);
 
@@ -53,12 +68,7 @@ public class SampleDataLoader implements CommandLineRunner {
 				.lastName("Ya")
 				.phoneNumber("010.0000.0000")
 				.email("ya@suo.com")
-				.address(Address.builder()
-						.address("Qst")
-						.city("Wind-City")
-						.state("QWER")
-						.zip("030")
-						.build())
+				.address(address3)
 				.build();
 		personRepository.save(person);
 	}
