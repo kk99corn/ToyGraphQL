f1d6fd54937fa6f230103d39d8b37f26becdbf2e
99 <hahahoho5915@gmail.com>
2022-09-20 19:16:04 +0900

    add GraphQl Config 추가

--- src/main/java/com/toy/graphql/cofig/GraphQlConfig.java
+++ src/main/java/com/toy/graphql/cofig/GraphQlConfig.java
@@ -1,4 +1,21 @@
 package com.toy.graphql.cofig;
 
+import graphql.schema.GraphQLScalarType;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+@Configuration
 public class GraphQlConfig {
+	@Bean
+	public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+		GraphQLScalarType jsonScalarType = GraphQLScalarType.newScalar()
+				.name("JSON")
+				.description("A JSON scalar")
+				.coercing(new JsonNodeCoercing())
+				.build();
+
+		return wiringBuilder -> wiringBuilder
+				.scalar(jsonScalarType);
+	}
 }
